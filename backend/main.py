51ff159586a6272bd5d5becc9aedd4a576a050d8
luisosio300905-lgtm <luisosio300905@gmail.com>
2026-07-06 21:43:19 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,8 +14,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://localhost:5173",
-    "http://127.0.0.1:5500",
-    "*"                     
+    "http://127.0.0.1:5500"                  
 ]
 
 app.add_middleware(
