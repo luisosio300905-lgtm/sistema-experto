0be4c6a1a9017e4284aee70c2c600bbb2dd11905
luisosio300905-lgtm <luisosio300905@gmail.com>
2026-07-06 21:17:17 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.endpoints import router as api_router
+from backend.app.api.endpoints import router as api_router
 
 app = FastAPI(
     title="Sistema Experto Psicoacadémico",
