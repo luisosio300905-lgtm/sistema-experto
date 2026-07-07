91575a5a6942dc9111961cfdb8098c32e5d9922d
luisosio300905-lgtm <luisosio300905@gmail.com>
2026-07-06 21:34:44 -0500

    Add localhost:8000 to allowed origins

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app = FastAPI(
 origins = [
     "https://sistema-experto-psicologia.vercel.app"
     "http://localhost:3000",
+    "http://localhost:8000",
     "http://localhost:5173",
     "http://127.0.0.1:5500",
     "*"                     
